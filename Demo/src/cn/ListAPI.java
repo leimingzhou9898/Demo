017e1e29676e72ef7b83c41397842c763a6b0853
leimingzhou9898 <1015230302@qq.com>
2017-10-07 18:50:56 +0800

    更新少量代码

--- Demo/src/cn/ListAPI.java
+++ Demo/src/cn/ListAPI.java
@@ -1,5 +1,35 @@
 package cn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListAPI {
+	public static void main(String[] args) {
+		StringBuilder buil= new StringBuilder();
+		String str = "";
+		ArrayList list=new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		for (int i = 0; i < list.size(); i++) {
+			buil.append(list.get(i)).append(",");
+			str += list.get(i)+",";
+		}
+		buil.deleteCharAt(buil.length()-1);
+		str.substring(0, str.length()-1);
+		System.out.println(buil.toString()+"\n"+str);
+	}
+	
+	
+	
+//	for (Map.Entry<String, String> entry : params.entrySet()) {
+//        url.append(entry.getKey()).append("=");
+//        url.append(URLEncoder.encode(entry.getValue(), encoding));// ±àÂë
+//        url.append('&');
+//    }
+//    url.deleteCharAt(url.length() - 1);
 
 }
--- Demo/src/cn/StringAPI.java
+++ Demo/src/cn/StringAPI.java
@@ -1,5 +1,13 @@
 package cn;
 
 public class StringAPI {
-
+	public static void main(String[] args) {
+		String str = null;
+		str += "str";
+		System.out.println(str);//½á¹ûÎª£ºnullstr
+		
+		
+	}
+	
+	
 }
